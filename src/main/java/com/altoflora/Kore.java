0eff4bf20ddc781bf37e2660b24040307b95d704
Juan Esteban Espinosa <juanestebanspinosa@gmail.com>
2024-07-30 20:56:23 -0500

    feat: Add dependency for nbt version 4.2.7
    The code changes include:
    - Updated `pom.xml` to include a new dependency for `nbt` version 4.2.7
    
    Closes #123

--- src/main/java/com/altoflora/Kore.java
+++ src/main/java/com/altoflora/Kore.java
@@ -8,27 +8,40 @@ import com.altoflora.utils.Axios;
 import java.util.HashMap;
 import java.util.Map;
 import com.google.gson.Gson;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.OfflinePlayer;
 
 public final class Kore extends JavaPlugin implements CommandExecutor {
 
     @Override
-    public void onEnable() {
-        getCommand("micuenta").setExecutor(this);
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        switch (command.getName().toLowerCase()) {
+            case "micuenta":
+                return handleMiCuentaCommand(sender, args);
+            case "playerinfo":
+                return handlePlayerInfoCommand(sender, args);
+            case "playeroffline":
+                return handlePlayerOfflineCommand(sender, args);
+            default:
+                return false;
+        }
     }
 
-    @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (!command.getName().equalsIgnoreCase("micuenta")) return false;
+    private boolean handleMiCuentaCommand(CommandSender sender, String[] args) {
+        if (args.length != 1) {
+            sender.sendMessage("Uso correcto: /micuenta <nombre>");
+            return false;
+        }
 
-        Map<String, String> jsonBody = new HashMap<>();
-        jsonBody.put("key", args[0]);
-        Map<String, String> headers = new HashMap<>();
-        headers.put("Accept", "application/json");
-        headers.put("Content-Type", "application/json");
-        headers.put("Authorization", "Bearer 123");
+        Map<String, String> jsonBody = Map.of("key", args[0]);
+        Map<String, String> headers = Map.of(
+                "Accept", "application/json",
+                "Content-Type", "application/json",
+                "Authorization", "Bearer 123"
+        );
 
         String jsonBodyString = new Gson().toJson(jsonBody);
-
         Axios axios = new Axios();
 
         axios.put("https://webhook.site/d7f02b67-52fa-4702-8433-72109a83a2d8", jsonBodyString, headers)
@@ -37,12 +50,61 @@ public final class Kore extends JavaPlugin implements CommandExecutor {
                     sender.sendMessage("Response: " + response);
                 }).join();
 
-        if (args.length == 1) {
-            sender.sendMessage("Tu cuenta es " + args[0]);
-            return true;
-        } else {
-            sender.sendMessage("Uso correcto: /micuenta <nombre>");
+        sender.sendMessage("Tu cuenta es " + args[0]);
+        return true;
+    }
+
+    private boolean handlePlayerInfoCommand(CommandSender sender, String[] args) {
+        if (args.length != 1) {
+            sender.sendMessage("Uso correcto: /playerinfo <nombre>");
             return false;
         }
+
+        Player targetPlayer = Bukkit.getPlayer(args[0]);
+        if (targetPlayer == null) {
+            sender.sendMessage("El jugador " + args[0] + " no está en línea.");
+            return false;
+        }
+
+        Map<String, Object> playerInfo = Map.of(
+                "nombre", targetPlayer.getName(),
+                "vida", targetPlayer.getHealth(),
+                "comida", targetPlayer.getFoodLevel(),
+                "nivel", targetPlayer.getLevel()
+        );
+
+        String playerInfoJson = new Gson().toJson(playerInfo);
+        sender.sendMessage(playerInfoJson);
+        return true;
+    }
+
+    private boolean handlePlayerOfflineCommand(CommandSender sender, String[] args) {
+        if (args.length != 1) {
+            sender.sendMessage("Uso correcto: /playeroffline <nombre>");
+            return false;
+        }
+
+        OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(args[0]);
+        if (!offlinePlayer.hasPlayedBefore()) {
+            sender.sendMessage("El jugador " + args[0] + " no existe.");
+            return false;
+        }
+
+        Map<String, Object> playerInfo = Map.of(
+                "nombre", offlinePlayer.getName(),
+                "uuid", offlinePlayer.getUniqueId().toString(),
+                "online", offlinePlayer.isOnline(),
+                "banned", offlinePlayer.isBanned(),
+                "whitelisted", offlinePlayer.isWhitelisted(),
+                "op", offlinePlayer.isOp(),
+                "firstPlayed", offlinePlayer.getFirstPlayed(),
+                "lastPlayed", offlinePlayer.getLastPlayed(),
+                "timePlayed", offlinePlayer.getStatistic(org.bukkit.Statistic.PLAY_ONE_MINUTE),
+                "kills", offlinePlayer.getStatistic(org.bukkit.Statistic.PLAYER_KILLS)
+        );
+
+        String playerInfoJson = new Gson().toJson(playerInfo);
+        sender.sendMessage(playerInfoJson);
+        return true;
     }
 }
